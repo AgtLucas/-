5afac33ab1c1fb50a90610556a007ebff1f03e15
AgtLucas <lucas@agtlucas.com>
2017-12-26 19:57:43 -0200

    Add playground

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate chrono;
+#[macro_use]
+extern crate yew;
+
+use chrono::prelude::*;
+use yew::html::*;
+
+struct Model {
+    value: i64,
+}
+
+enum Msg {
+    Increment,
+    Decrement,
+}
+
+fn update(_: &mut Context<Msg>, model: &mut Model, msg: Msg) {
+    match msg {
+        Msg::Increment => {
+            model.value = model.value + 1;
+        }
+
+        Msg::Decrement => {
+            model.value = model.value - 1;
+        }
+    }
+}
+
+fn view(model: &Model) -> Html<Msg> {
+    html! {
+        <div>
+            <button onclick=|_| Msg::Increment,>{ "Increment" }</button>
+            <button onclick=|_| Msg::Decrement,>{ "Decrement" }</button>
+            <p>{ model.value }</p>
+            <p>{ Local::now() }</p>
+        </div>
+    }
+}
+
+fn main() {
+    let model = Model {
+        value: 0,
+    };
+    program(model, update, view);
+}
